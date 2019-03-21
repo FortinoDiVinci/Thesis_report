88141b63a3680fc972af80656d2f47d6ff3ab23c
FortinoDiVinci <Vincent@MacBook-Pro-de-Vincent.local>
2019-03-21 14:15:31 +0100

    Update youbot_joints_limits.py
    Functionnal controller to check velocity commands are consistant with the robot position limits

--- catkin_ws/src/youbot_ros_control/scripts/youbot_joints_limits.py
+++ catkin_ws/src/youbot_ros_control/scripts/youbot_joints_limits.py
@@ -1,37 +1,68 @@
 #!/usr/bin/env python
-# Code created by Vincent FORTINEAU (vincent.fortineau@centralesupelec.fr).
+# This program is dedicated to the kuka youbot, without base nor gripper
+# therefore a 5 axis robots communicating with this script throught ROS topics
+
+__version__ = '0.1'
+__author__ = 'Vincent FORTINEAU <vincent.fortineau@centralesupelec.fr>'
+
+########################
+#  LIBRARIES
+########################
 
 import rospy
+import time 
+import threading
 
 from brics_actuator.msg import JointPositions, JointVelocities, JointValue
 from sensor_msgs.msg import JointState
 
-import sys, select, termios, tty, signal, time, threading
-
+########################
+#  CONST & GLOBAL VAR
+########################
 
 # thetas[i] = [pos_i, vel_i, torq_i]
 thetas = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-numberOfJoints = 5
 init_flag = False
+# software limitation to work in specified space
+joint_upper_limit = [0.5, 1.76, -0.95, 2.84, 5.6]
+joint_lower_limit = [0.02, 1.35, -2.06, 1.31, 0.12]
+#
+out_of_bound = [0, 0, 0, 0, 0]
+speed_sign = [0, 0, 0, 0, 0]
 
+########################
+#  FUNCTIONS
+########################
 
-def getJointState(joints):
-    global thetas
+# Callback function
+
+def getJointStateCallback(joints):
+    global thetas, joint_upper_limit, joint_lower_limit, out_of_bound, speed_sign
     try:
-        for theta in xrange(0, numberOfJoints):    
+        for theta in xrange(len(thetas)):    
             thetas[theta][0] = joints.position[theta]
             thetas[theta][1] = joints.velocity[theta]   
-            thetas[theta][2] = joints.effort[theta]    
-               
+            thetas[theta][2] = joints.effort[theta] 
+            
+            
+            speed_sign[theta] = sign(joints.velocity[theta])
+
+            if joints.position[theta] >= joint_upper_limit[theta]:
+                out_of_bound[theta] = 1
+            elif joints.position[theta] <= joint_lower_limit[theta]: 
+                out_of_bound[theta] = -1
+            else:
+                out_of_bound[theta] = 0
+          
     except:
         print("An error occured while reading the robot joint state")
-        
+
+# Callback function       
 
 def getJointStateOnce(joints):
     global thetas, init_flag, sub
     try:
-        for theta in xrange(0, numberOfJoints):    
+        for theta in xrange(len(thetas)):    
             thetas[theta][0] = joints.position[theta]
             thetas[theta][1] = joints.velocity[theta]   
             thetas[theta][2] = joints.effort[theta]    
@@ -40,6 +71,8 @@ def getJointStateOnce(joints):
         print("An error occured while reading the robot joint state")    
     sub.unregister()
     init_flag = True
+
+#
 
 def subJointStateOnce():
     # Get initial values and set them to thetas
@@ -56,10 +89,12 @@ def subJointStateOnce():
             return -1	
     return 0
 
-def functionState(pub, conf):
+# Publish joints position in predefined states
+
+def operatingState(pub, conf):
     
     if conf == 1:
-        func_pos = [0.02, 1.76, -2.06, 2.84, 0.12]
+        func_pos = [0.5, 1.76, -2.06, 2.84, 0.12]
     elif conf == 2:
         func_pos = [0.02, 1.35, -0.95, 1.31, 5.60]
     else:
@@ -80,6 +115,8 @@ def functionState(pub, conf):
     
     return True
 
+# Publish joints position in initial state
+
 def initState(pub):
     
     init_pos = [0.01007, 0.01007, -0.0158, 0.02213, 0.11062]
@@ -98,67 +135,160 @@ def initState(pub):
     
     return True
 
-def pub_delay():
+# Dummy delay that experimentaly seems necessary after publishing joint
+# position
+
+def pubDelay():
     for wait in xrange(0,3):
         time.sleep(0.1)    
 
+#
 
 def isMoving():
     nul_th_count = 0  
     
-    for theta in xrange(0, numberOfJoints): 
+    for theta in xrange(len(thetas)): 
         if thetas[theta][1] < 0.01 and thetas[theta][1] > -0.01:
             nul_th_count = nul_th_count + 1
             
-    if nul_th_count == numberOfJoints:               
+    if nul_th_count == len(thetas):               
         return False    
     else:
         return True
 
+#
+
+def sign(x):
+    
+    if (x == 0): 
+        return 0
+    elif (x < 0):
+        return -1
+    else:
+        return 1
+# 
+
 def checkPosThread():
     th = threading.currentThread()
-    global thetas  # TODO a cleaner way should be used
+    global thetas  # TODO a cleaner way should be used ?
         
     while getattr(th, "stop", False) == False:
-        for theta in thetas:
-            if theta[0] > 2:
-                print("outside limits")
+        for theta_i in xrange(len(thetas)):
+            if (thetas[theta_i][0] <= joint_upper_limit[theta_i] and
+                thetas[theta_i][0] >= joint_lower_limit[theta_i]):
+                # this joint is okay
+                # do_something()
+                pass
+            else:
+                print("The command required is out of the working \
+                        space: %")
             
-    print("Exiting Position checking thread...")
+    print("\nExiting Position checking thread...")
+
+#
+
+def checkPosCallback(joint_vel_cmd):
+    global pub_vel, thetas, out_of_bound, speed_sign
+	
+    not_generated_cmd = [0, 1, 2, 3, 4]
+   
+    for cmd in joint_vel_cmd.velocities:
+        try:
+            joint_nb = int(cmd.joint_uri[-1]) - 1
+        except:
+            print("joint uri (name) is not properly defined." \
+                  "\n %s was given" % cmd.joint_uri)
+        else:
+            not_generated_cmd.remove(joint_nb) # remove by value
+            # this case test if joint is out of bound and if out_of_boud 
+            # and speed_sign have the same sign
+            if (out_of_bound[joint_nb] != 0 and
+                out_of_bound[joint_nb] + sign(cmd.value) != 0):  
+                #
+                cmd.value = 0.
+                rospy.logwarn("Joint %d is out of bound", (joint_nb+1))
+                
+    for joint_cmd in not_generated_cmd:
+        if (out_of_bound[joint_cmd] != 0 and speed_sign[joint_cmd] != 0 and
+            out_of_bound[joint_cmd] + speed_sign[joint_cmd] != 0):
+            #
+            joint_vel_cmd.velocities.append(
+                createJointValueVel(0., joint_cmd + 1))
+            rospy.logwarn("Joint %d is out of bound (no cmd)", (joint_cmd + 1))
+            
+    pub_vel.publish(joint_vel_cmd)
+    #pubDelay()
+    #print(joint_vel_cmd)
+    
+#
+
+def createJointValueVel(val, joint_nb):
+    j_v = JointValue()
+    j_v.joint_uri = "arm_joint_" + str(joint_nb)
+    j_v.value = val
+    j_v.unit = "s^-1 rad" # velocity unit
+
+    return j_v
+
+########################
+#  MAIN
+########################
     
 if __name__=="__main__":
     
     rospy.init_node('teleop_joint_control')
 	
-    pub_pos = rospy.Publisher('arm_1/arm_controller/position_command', JointPositions, queue_size=1)
-    pub_vel = rospy.Publisher('arm_1/arm_controller/velocity_command', JointVelocities, queue_size=1)
+    """ROS Publisher, Subscribers and Services"""
+	
+    pub_pos = rospy.Publisher('arm_1/arm_controller/position_command',
+                               JointPositions, queue_size=1)
+    pub_vel = rospy.Publisher('arm_1/arm_controller/velocity_command',
+                               JointVelocities, queue_size=1)
     
-    sub = None
+    sub = None 
+    sub_state = rospy.Subscriber("joint_states", JointState,
+                                 getJointStateCallback)
     
-    sub_state = rospy.Subscriber("joint_states", JointState, getJointState)
+    """Necessary dummy operation to initiate communication"""
 
     # Dummy publishing 
     armJointPos = JointPositions()      
     pub_pos.publish(armJointPos)
-    # Dumy delay
-    for wait in xrange(0,3):
-        time.sleep(0.1)
+    pubDelay()
     
-    is_Moving = functionState(pub_pos, 2)
-    pub_delay()
+    """Initialisation"""
+    
+    is_Moving = operatingState(pub_pos, 2)
+    pubDelay()
         
     while(isMoving()):
         pass
     is_Moving = False
      
     is_Moving = initState(pub_pos)
-    pub_delay()
+    pubDelay()
     
-    th = threading.Thread(target=checkPosThread)
-    th.start()
+    """Main operations"""
+    
+    sub_cmd = rospy.Subscriber("joint_vel_cmd/", JointVelocities,
+                                checkPosCallback)
+                                
+    #th = threading.Thread(target=checkPosThread, args=(,))
+    #th.start()
     
     rospy.spin()
     
-    th.stop = True
-    th.join()
+    #th.stop = True
+    #th.join()
+    
+    sub_cmd.unregister()
+    
+    is_Moving = initState(pub_pos)
+    pubDelay()
+    
+    while(isMoving()):
+        pass
+    is_Moving = False   
+    
+    sub_state.unregister()
     
