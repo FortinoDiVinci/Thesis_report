77160007b4678b78be99cf641fb2b7e9237f77d0
Vincent Fortineau <vfortineau@free.fr>
2019-07-26 15:33:11 +0200

    ball_simulator update
    - paddle height correctly tracked
    - paddle speed corrected

--- catkin_ws/src/ball_simulator/include/ball_simulator/logger/message_aggregator.h
+++ catkin_ws/src/ball_simulator/include/ball_simulator/logger/message_aggregator.h
@@ -6,52 +6,49 @@
 #include <geometry_msgs/WrenchStamped.h>
 #include <ros/console.h>
 #include <sensor_msgs/JointState.h>
-#include <string>
 #include <tf/transform_listener.h>
+#include <string>
 
-namespace ball_simulator {
+namespace ball_simulator
+{
 class MessageAggregator
 {
 public:
-    MessageAggregator(
-        const std::string& world_frame_id,
-        const std::string& arm_frame_id,
-        const std::string& virtual_paddle_frame_id,
-        const std::string& ball_frame_id);
+  MessageAggregator(const std::string& world_frame_id, const std::string& arm_frame_id,
+                    const std::string& virtual_paddle_frame_id, const std::string& ball_frame_id);
 
-    void onRealPaddlePose(const geometry_msgs::PoseStamped::ConstPtr& msg);
+  void onRealPaddlePose(const geometry_msgs::PoseStamped::ConstPtr& msg);
 
-    void onVirtualPaddlePose(const geometry_msgs::PoseStamped::ConstPtr& msg);
+  void onVirtualPaddlePose(const geometry_msgs::PoseStamped::ConstPtr& msg);
 
-    void onBallPose(const geometry_msgs::PoseStamped::ConstPtr& msg);
+  void onBallPose(const geometry_msgs::PoseStamped::ConstPtr& msg);
 
-    void onForceFeedback(const geometry_msgs::WrenchStamped::ConstPtr& msg);
+  void onForceFeedback(const geometry_msgs::WrenchStamped::ConstPtr& msg);
 
-    void onArmJointStates(const sensor_msgs::JointState::ConstPtr& msg);
+  void onArmJointStates(const sensor_msgs::JointState::ConstPtr& msg);
 
-    void logFrameHeightField(
-        LogEntry& entry, const ros::Time& stamp, const std::string& frame_id);
+  void logFrameHeightField(LogEntry& entry, const ros::Time& stamp, const std::string& frame_id);
 
-    std::string produceLogEntry();
+  std::string produceLogEntry();
 
 protected:
-    template <typename T>
-    void addField(std::stringstream& stream, const T& field);
+  template <typename T>
+  void addField(std::stringstream& stream, const T& field);
 
-    void addEmptyField(std::stringstream& stream);
+  void addEmptyField(std::stringstream& stream);
 
 private:
-    geometry_msgs::PoseStamped::ConstPtr real_paddle_pose;
-    geometry_msgs::PoseStamped::ConstPtr virtual_paddle_pose;
-    geometry_msgs::PoseStamped::ConstPtr ball_pose;
-    geometry_msgs::WrenchStamped::ConstPtr force_feedback;
-    sensor_msgs::JointState::ConstPtr arm_joint_states;
-    tf::TransformListener tf_listener;
-    std::string world_frame_id;
-    std::string arm_frame_id;
-    std::string virtual_paddle_frame_id;
-    std::string ball_frame_id;
+  geometry_msgs::PoseStamped::ConstPtr real_paddle_pose;
+  geometry_msgs::PoseStamped::ConstPtr virtual_paddle_pose;
+  geometry_msgs::PoseStamped::ConstPtr ball_pose;
+  geometry_msgs::WrenchStamped::ConstPtr force_feedback;
+  sensor_msgs::JointState::ConstPtr arm_joint_states;
+  tf::TransformListener tf_listener;
+  std::string world_frame_id;
+  std::string arm_frame_id;
+  std::string virtual_paddle_frame_id;
+  std::string ball_frame_id;
 };
-} // namespace ball_simulator
+}  // namespace ball_simulator
 
 #endif
--- catkin_ws/src/ball_simulator/src/logger/message_aggregator.cpp
+++ catkin_ws/src/ball_simulator/src/logger/message_aggregator.cpp
@@ -1,144 +1,134 @@
 #include <ball_simulator/logger/message_aggregator.h>
 
-namespace ball_simulator {
-
-MessageAggregator::MessageAggregator(
-    const std::string& world_frame_id,
-    const std::string& arm_frame_id,
-    const std::string& virtual_paddle_frame_id,
-    const std::string& ball_frame_id)
-    : world_frame_id(world_frame_id),
-      arm_frame_id(arm_frame_id),
-      virtual_paddle_frame_id(virtual_paddle_frame_id),
-      ball_frame_id(ball_frame_id)
-{}
-
-void MessageAggregator::onRealPaddlePose(
-    const geometry_msgs::PoseStamped::ConstPtr& msg)
+namespace ball_simulator
 {
-    real_paddle_pose = msg;
+MessageAggregator::MessageAggregator(const std::string& world_frame_id, const std::string& arm_frame_id,
+                                     const std::string& virtual_paddle_frame_id, const std::string& ball_frame_id)
+  : world_frame_id(world_frame_id)
+  , arm_frame_id(arm_frame_id)
+  , virtual_paddle_frame_id(virtual_paddle_frame_id)
+  , ball_frame_id(ball_frame_id)
+{
 }
 
-void MessageAggregator::onVirtualPaddlePose(
-    const geometry_msgs::PoseStamped::ConstPtr& msg)
+void MessageAggregator::onRealPaddlePose(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-    virtual_paddle_pose = msg;
+  real_paddle_pose = msg;
 }
 
-void MessageAggregator::onBallPose(
-    const geometry_msgs::PoseStamped::ConstPtr& msg)
+void MessageAggregator::onVirtualPaddlePose(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-    ball_pose = msg;
+  virtual_paddle_pose = msg;
 }
 
-void MessageAggregator::onForceFeedback(
-    const geometry_msgs::WrenchStamped::ConstPtr& msg)
+void MessageAggregator::onBallPose(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-    force_feedback = msg;
+  ball_pose = msg;
 }
 
-void MessageAggregator::onArmJointStates(
-    const sensor_msgs::JointState::ConstPtr& msg)
+void MessageAggregator::onForceFeedback(const geometry_msgs::WrenchStamped::ConstPtr& msg)
 {
-    arm_joint_states = msg;
+  force_feedback = msg;
 }
 
-void MessageAggregator::logFrameHeightField(
-    LogEntry& entry, const ros::Time& stamp, const std::string& frame_id)
+void MessageAggregator::onArmJointStates(const sensor_msgs::JointState::ConstPtr& msg)
 {
-    tf::StampedTransform transform;
+  arm_joint_states = msg;
+}
 
-    try
-    {
-        tf_listener.lookupTransform(frame_id, world_frame_id, stamp, transform);
-        entry << transform.getOrigin().getZ();
-    }
-    catch (const tf::TransformException& e)
-    {
-        ROS_WARN_THROTTLE(
-            2.0, "cannot compute %s coordinates: %s", frame_id.c_str(),
-            e.what());
-        entry << LogEntry::EmptyField();
-    }
+void MessageAggregator::logFrameHeightField(LogEntry& entry, const ros::Time& stamp, const std::string& frame_id)
+{
+  tf::StampedTransform transform;
+
+  try
+  {
+    tf_listener.lookupTransform(frame_id, world_frame_id, stamp, transform);
+    entry << transform.getOrigin().getZ();
+  }
+  catch (const tf::TransformException& e)
+  {
+    ROS_WARN_THROTTLE(2.0, "cannot compute %s coordinates: %s", frame_id.c_str(), e.what());
+    entry << LogEntry::EmptyField();
+  }
 }
 
 std::string MessageAggregator::produceLogEntry()
 {
-    LogEntry entry;
-    ros::Time stamp = ros::Time::now() - ros::Duration(0.1);
-    entry << stamp;
+  LogEntry entry;
+  ros::Time stamp = ros::Time::now() - ros::Duration(0.1);
+  entry << stamp;
 
-    logFrameHeightField(entry, stamp, arm_frame_id);
-    logFrameHeightField(entry, stamp, virtual_paddle_frame_id);
-    logFrameHeightField(entry, stamp, ball_frame_id);
+  logFrameHeightField(entry, stamp, arm_frame_id);
+  logFrameHeightField(entry, stamp, virtual_paddle_frame_id);
+  logFrameHeightField(entry, stamp, ball_frame_id);
 
-    if (real_paddle_pose)
+  if (real_paddle_pose)
+  {
+    entry << real_paddle_pose->pose.position.z;
+  }
+  else
+  {
+    entry << LogEntry::EmptyField();
+    ROS_WARN_THROTTLE(2.0, "no real paddle pose received yet");
+  }
+
+  if (force_feedback)
+  {
+    entry << force_feedback->wrench.force.z;
+  }
+  else
+  {
+    entry << LogEntry::EmptyField();
+    ROS_WARN_THROTTLE(2.0, "no force feedback received yet");
+  }
+
+  if (arm_joint_states)
+  {
+    for (int i = 0; i < arm_joint_states->name.size(); ++i)
     {
-        entry << real_paddle_pose->pose.position.z;
-    }
-    else
-    {
+      if (i < arm_joint_states->position.size())
+      {
+        entry << arm_joint_states->position[i];
+      }
+      else
+      {
         entry << LogEntry::EmptyField();
-        ROS_WARN_THROTTLE(2.0, "no real paddle pose received yet");
-    }
-
-    if (force_feedback)
-    {
-        entry << force_feedback->wrench.force.z;
-    }
-    else
-    {
+      }
+      if (i < arm_joint_states->velocity.size())
+      {
+        entry << arm_joint_states->velocity[i];
+      }
+      else
+      {
         entry << LogEntry::EmptyField();
-        ROS_WARN_THROTTLE(2.0, "no force feedback received yet");
+      }
+      if (i < arm_joint_states->effort.size())
+      {
+        entry << arm_joint_states->effort[i];
+      }
+      else
+      {
+        entry << LogEntry::EmptyField();
+      }
     }
+  }
+  else
+  {
+    ROS_WARN_THROTTLE(2.0, "no arm joint states received yet");
+  }
 
-    if (arm_joint_states)
-    {
-        for (int i = 0; i < arm_joint_states->name.size(); ++i)
-        {
-            if (i < arm_joint_states->position.size())
-            {
-                entry << arm_joint_states->position[i];
-            }
-            else
-            {
-                entry << LogEntry::EmptyField();
-            }
-            if (i < arm_joint_states->velocity.size())
-            {
-                entry << arm_joint_states->velocity[i];
-            }
-            else
-            {
-                entry << LogEntry::EmptyField();
-            }
-            if (i < arm_joint_states->effort.size())
-            {
-                entry << arm_joint_states->effort[i];
-            }
-            else
-            {
-                entry << LogEntry::EmptyField();
-            }
-        }
-    }
-    else
-    {
-        ROS_WARN_THROTTLE(2.0, "no arm joint states received yet");
-    }
-
-    return entry.str();
+  return entry.str();
 }
 
 template <typename T>
 void MessageAggregator::addField(std::stringstream& stream, const T& field)
 {
-    stream << field << ", ";
+  stream << field << ", ";
 }
 
 void MessageAggregator::addEmptyField(std::stringstream& stream)
 {
-    stream << ", ";
+  stream << ", ";
 }
 
-} // namespace ball_simulator
+}  // namespace ball_simulator
