3ae975a0057ee1c6651804ef73b2ac3cc45f1540
Vincent Fortineau <vfortineau@free.fr>
2020-01-15 15:07:14 +0100

    Data for joint & robot identification

--- data/youBot_analysis/PID_gain/pid_gain analysis.m
+++ data/youBot_analysis/PID_gain/pid_gain analysis.m
@@ -2,16 +2,18 @@ clear all
 
 %% identification for joint 5
 
-velocity_cmd = readtable('bagfile-_arm_1_arm_controller_velocity_command.csv');
-joint_set_points = readtable('bagfile-_arm_1_joint_set_points.csv');
-joint_states = readtable('bagfile-_joint_states.csv');
+%velocity_cmd = readtable('bagfile-_arm_1_arm_controller_velocity_command.csv');
+joint_set_points = readtable('2020_01_14/bagfile-_arm_1_joint_set_points.csv');
+joint_states = readtable('2020_01_14/bagfile-_joint_states.csv');
 
 t = joint_states.x_time;
-vq5 = joint_states.field_velocity4;
-q5 = joint_states.field_position4;
-vq5_cmd = interp1(velocity_cmd.x_time, velocity_cmd.field_velocities0_value, joint_states.x_time);
-eq5_sp = interp1(joint_set_points.x_time, joint_set_points.field_effort4, joint_states.x_time);
-vq5_sp = interp1(joint_set_points.x_time, joint_set_points.field_velocity4, joint_states.x_time);
+vq5 = joint_states.field_velocity3;
+eq5 = joint_states.field_effort3;
+q5 = joint_states.field_position3;
+%vq5_cmd = interp1(velocity_cmd.x_time, velocity_cmd.field_velocities0_value, joint_states.x_time);
+eq5_sp = interp1(joint_set_points.x_time, joint_set_points.field_effort3, joint_states.x_time);
+vq5_sp = interp1(joint_set_points.x_time, joint_set_points.field_velocity3, joint_states.x_time);
+epsilon_vq5 = vq5_sp - vq5;
 
 p_gain = 800/256;
 torque_constant = 0.049; %Nm/A
@@ -44,3 +46,28 @@ hold on, grid on
 plot((t-t(1))*1e-9, eq5_sp/torque_constant);
 plot((t-t(1))*1e-9, epsilon);
 legend('H(p)', 'I', '\epsilon')
+
+%%
+figure
+plot((t-t(1))*1e-9, vq5_sp);
+hold on, grid on
+plot((t-t(1))*1e-9, eq5_sp);
+plot((t-t(1))*1e-9, vq5);
+plot((t-t(1))*1e-9, eq5);
+plot((t-t(1))*1e-9, epsilon_vq5);
+legend('dq_c(4)', '\tau_c(4)', 'dq(4)', '\tau(4)', '\epsilon(5)')
+
+s = tf('s');
+
+Kp = 1.269;
+Ki = 38.64;
+C = (Kp*s + Ki)/s;
+C_d = c2d(C, 0.001, 'tustin');
+[y,t1]=lsim(C_d,epsilon_vq5);
+
+figure
+plot((t-t(1))*1e-9, eq5);
+hold on, grid on
+plot(t1, y);
+plot((t-t(1))*1e-9, epsilon_vq5);
+legend('\tau','est','\dot(q)')
--- data/youBot_analysis/Robot_identification/test.m
+++ data/youBot_analysis/Robot_identification/test.m
@@ -19,24 +19,49 @@ tc_jnt4 = 0.051; % Nm/A
 
 %% experimental data
 
-experimental_joint_positions = readtable('joint_state_pos_traj_0');
-experimental_joint_sp_positions = readtable('joint_state_pos_traj_sp_0');
+% experimental_joint_positions = readtable('rosbag_vs_python_recording/joint_state_pos_traj_0');
+% experimental_joint_sp_positions = readtable('rosbag_vs_python_recording/joint_state_pos_traj_sp_0');
+% 
+% experimental_joint_velocities = readtable('rosbag_vs_python_recording/joint_state_vel_traj_0');
+% experimental_joint_sp_velocities = readtable('rosbag_vs_python_recording/joint_state_vel_traj_sp_0');
+% 
+% experimental_joint_efforts = readtable('rosbag_vs_python_recording/joint_state_eff_traj_0'); % signal should be filtered before recording
+% experimental_joint_sp_efforts = readtable('rosbag_vs_python_recording/joint_state_eff_traj_sp_0'); % signal should be filtered before recording
+% 
+% rosbag_joint_states = readtable('rosbag_vs_python_recording/bagfile-_joint_states.csv');
+% rosbag_joint_set_points = readtable('rosbag_vs_python_recording/bagfile-_arm_1_joint_set_points.csv');
+% 
+% pos_m = [rosbag_joint_states.field_position1 rosbag_joint_states.field_position2 rosbag_joint_states.field_position3];
+% vel_m = [rosbag_joint_states.field_velocity1 rosbag_joint_states.field_velocity2 rosbag_joint_states.field_velocity3];
+% eff_m = [rosbag_joint_states.field_effort1 rosbag_joint_states.field_effort2 rosbag_joint_states.field_effort3];
+% 
+% pos_sp = [rosbag_joint_set_points.field_position1 rosbag_joint_set_points.field_position2 rosbag_joint_set_points.field_position3];
+% vel_sp = [rosbag_joint_set_points.field_velocity1 rosbag_joint_set_points.field_velocity2 rosbag_joint_set_points.field_velocity3];
+% eff_sp = [rosbag_joint_set_points.field_effort1 rosbag_joint_set_points.field_effort2 rosbag_joint_set_points.field_effort3];
 
-experimental_joint_velocities = readtable('joint_state_vel_traj_0');
-experimental_joint_sp_velocities = readtable('joint_state_vel_traj_sp_0');
+rosbag_joint_states = readtable('rosbag_1kHz/bagfile-_joint_states.csv');
+rosbag_joint_set_points = readtable('rosbag_1kHz/bagfile-_arm_1_joint_set_points.csv');
 
-experimental_joint_efforts = readtable('joint_state_eff_traj_0');
-experimental_joint_sp_efforts = readtable('joint_state_eff_traj_sp_0');
+%%%%%%%%%%%
+%% DISPLAY
+%%%%%%%%%%%
 
 % measured joints positions
 
 figure()
 hold on, grid on
-t = (experimental_joint_positions.time_stamp - experimental_joint_positions.time_stamp(1))*10e-10;
-plot(t, experimental_joint_positions.arm_joint_2_pos)
-plot(t, experimental_joint_positions.arm_joint_3_pos)
-plot(t, experimental_joint_positions.arm_joint_4_pos)
-legend('joint2', 'joint3', 'joint4')
+%t = (experimental_joint_positions.time_stamp - experimental_joint_positions.time_stamp(1))*10e-10;
+%t_ros = (rosbag_joint_states.x_time - experimental_joint_positions.time_stamp(1))*10e-10;
+t_ros = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+%plot(t, experimental_joint_positions.arm_joint_2_pos)
+%plot(t, experimental_joint_positions.arm_joint_3_pos)
+%plot(t, experimental_joint_positions.arm_joint_4_pos)
+plot(t_ros, rosbag_joint_states.field_position1)
+plot(t_ros, rosbag_joint_states.field_position2)
+plot(t_ros, rosbag_joint_states.field_position3)
+
+%legend('joint2', 'joint3', 'joint4', 'joint2 ROS', 'joint3 ROS', 'joint4 ROS')
+legend('joint2 ROS', 'joint3 ROS', 'joint4 ROS')
 title('Joint State')
 
 % joints set point positions
@@ -55,10 +80,15 @@ title('Joint Setpoint data')
 figure()
 hold on, grid on
 t = (experimental_joint_efforts.time_stamp - experimental_joint_efforts.time_stamp(1))*10e-10;
+t_ros = (rosbag_joint_states.x_time - experimental_joint_positions.time_stamp(1))*10e-10;
 plot(t,experimental_joint_efforts.arm_joint_2_eff)
 plot(t,experimental_joint_efforts.arm_joint_3_eff)
 plot(t,experimental_joint_efforts.arm_joint_4_eff)
-legend('joint2', 'joint3', 'joint4')
+plot(t_ros, rosbag_joint_states.field_effort1)
+plot(t_ros, rosbag_joint_states.field_effort2)
+plot(t_ros, rosbag_joint_states.field_effort3)
+
+legend('joint2', 'joint3', 'joint4', 'joint2 ROS', 'joint3 ROS', 'joint4 ROS')
 title('Joint effort data')
 
 % joints set point effort
@@ -71,3 +101,76 @@ plot(t,experimental_joint_sp_efforts.arm_joint_3_eff)
 plot(t,experimental_joint_sp_efforts.arm_joint_4_eff)
 legend('joint2', 'joint3', 'joint4')
 title('Joint effort set point')
+
+%%%%%%%%%%%%%%%%%%%%
+% set point VS meas.
+%%%%%%%%%%%%%%%%%%%%
+
+%% joint2
+
+figure()
+hold on, grid on
+t_ros_js = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_js, rosbag_joint_states.field_position1)
+plot(t_ros_sp, rosbag_joint_set_points.field_position1)
+legend('meas', 'set point')
+title('Joint 2 position')
+
+figure()
+hold on, grid on
+t_ros_js = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_js, rosbag_joint_states.field_velocity1)
+plot(t_ros_sp, rosbag_joint_set_points.field_velocity1)
+legend('meas', 'set point')
+title('Joint 2 velocity')
+
+figure()
+hold on, grid on
+t_ros_js = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_js, rosbag_joint_states.field_effort1)
+plot(t_ros_sp, rosbag_joint_set_points.field_effort1)
+legend('meas', 'set point')
+title('Joint 2 effort')
+
+%% joint3
+
+figure()
+hold on, grid on
+t_ros_js = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_js, rosbag_joint_states.field_position2)
+plot(t_ros_sp, rosbag_joint_set_points.field_position2)
+legend('meas', 'set point')
+title('Joint 3 position')
+
+figure()
+hold on, grid on
+t_ros_js = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_js, rosbag_joint_states.field_velocity2)
+plot(t_ros_sp, rosbag_joint_set_points.field_velocity2)
+legend('meas', 'set point')
+title('Joint 3 velocity')
+
+figure()
+hold on, grid on
+t_ros_js = (rosbag_joint_states.x_time - rosbag_joint_states.x_time(1))*10e-10;
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_js, rosbag_joint_states.field_effort2)
+plot(t_ros_sp, rosbag_joint_set_points.field_effort2)
+legend('meas', 'set point')
+title('Joint 3 effort')
+
+%%%
+%% identification des gains du controlleur de vitesse
+%%%
+
+H = rosbag_joint_set_points.field_effort1./rosbag_joint_set_points.field_velocity1;
+
+figure()
+hold on, grid on
+t_ros_sp = (rosbag_joint_set_points.x_time - rosbag_joint_set_points.x_time(1))*10e-10;
+plot(t_ros_sp, H)
