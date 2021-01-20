3814665c14659d715f6df51e1529f2730e03df95
Vincent Fortineau <vfortineau@free.fr>
2021-01-20 14:47:49 +0100

    PID gain analysis update

--- data/youBot_analysis/PID identification/pid_gain_analysis.m
+++ data/youBot_analysis/PID identification/pid_gain_analysis.m
@@ -29,12 +29,19 @@ dth_sp = [interp1(joint_sp_t, joint_set_points.field_velocity0, t), ...
           interp1(joint_sp_t, joint_set_points.field_velocity3, t), ...
           interp1(joint_sp_t, joint_set_points.field_velocity4, t)];
 
+% /!\ eff_sp was define so that is is equal to i_sp./ Gear_ratio.* Tq_cnst
 eff_sp = [interp1(joint_sp_t, joint_set_points.field_effort0, t), ...
           interp1(joint_sp_t, joint_set_points.field_effort1, t), ...
           interp1(joint_sp_t, joint_set_points.field_effort2, t), ...
           interp1(joint_sp_t, joint_set_points.field_effort3, t), ...
           interp1(joint_sp_t, joint_set_points.field_effort4, t)];
 
+eff = [interp1(joint_t, joint_states.field_effort0, t), ...
+       interp1(joint_t, joint_states.field_effort1, t), ...
+       interp1(joint_t, joint_states.field_effort2, t), ...
+       interp1(joint_t, joint_states.field_effort3, t), ...
+       interp1(joint_t, joint_states.field_effort4, t)];
+      
 eps_dth = dth_sp - dth;
       
 p_gain = [0,2500,1500,2000,0];
@@ -78,16 +85,47 @@ C4_d = c2d(C4, 1e-3, 'tustin');
 [y3,t3]=lsim(C3_d,eps_dth(:,3));
 [y4,t4]=lsim(C4_d,eps_dth(:,4));
 
+% analysis in continuous time brings the exact same result
+%[y2c,t2c]=lsim(C2,eps_dth(:,2),t);
+%[y3c,t3c]=lsim(C3,eps_dth(:,3),t);
+%[y4c,t4c]=lsim(C4,eps_dth(:,4),t);
+
 i_cmd = [y2, y3, y4];
 eff_e = i_cmd.*torque_constant(2:4)./gear_ratio(2:4);
+i_sp = eff_sp./torque_constant.*gear_ratio;
 ts = [t2, t3, t4];
 
-figure
-plot(ts,eff_e)
+%ic_cmd = [y2c, y3c, y4c];
+%eff_ec = ic_cmd.*torque_constant(2:4)./gear_ratio(2:4);
+
+figure('DefaultAxesFontSize',18)
+subplot(3,1,1)
+title('Current estimated using robot gain divided by counter VS real')
 hold on, grid on
-plot(t, eff_sp(:,[2,3,4])-mean(eff_sp(:,[2,3,4])));
-legend('\tau_{e2}', '\tau_{e2}', '\tau_{e4}', '\tau_2', '\tau_3', '\tau_4')
-title('Effort estimated using robot gain divided by counter VS real')
+plot(ts(1:3e4),i_cmd(1:3e4,3))
+plot(t(1:3e4), i_sp(1:3e4,4)-mean(i_sp(1:3e4,4)));
+plot(t(1:3e4), eff(1:3e4,4)-mean(eff(1:3e4,4)));
+plot(t(1:3e4), (i_sp(1:3e4,4)-mean(i_sp(1:3e4,4)))*torque_constant(4)/gear_ratio(4));
+legend('i_{e4}', 'i_4', '\tau_m', 'i_4/Tc*Gr')
+ylabel('Current (A)')
+subplot(3,1,2)
+hold on, grid on
+plot(ts(3e4:6e4),i_cmd(3e4:6e4,2))
+plot(t(3e4:6e4), i_sp(3e4:6e4,3)-mean(i_sp(3e4:6e4,3)));
+legend('i_{e3}', 'i_3')
+ylabel('Current (A)')
+subplot(3,1,3)
+hold on, grid on
+plot(ts(6e4:7.9e4),i_cmd(6e4:7.9e4,1))
+plot(t(6e4:7.9e4), i_sp(6e4:7.9e4,2)-mean(i_sp(6e4:7.9e4,2)));
+xlabel('Time (s)')
+ylabel('Current (A)')
+legend('i_{e2}', 'i_2')
+%plot(ts,eff_ec, '--')
+%plot(t, i_sp(:,[2,3,4])-mean(i_sp(:,[2,3,4])));%
+%legend('\tau_{e2}', '\tau_{e2}', '\tau_{e4}', '\tau_{ec2}', '\tau_{ec2}', '\tau_{ec4}', '\tau_2', '\tau_3', '\tau_4')
+%legend('i_{e2}', 'i_{e2}', 'i_{e4}', 'i_2', 'i_3', 'i_4')
+
 
 %% identification
 
