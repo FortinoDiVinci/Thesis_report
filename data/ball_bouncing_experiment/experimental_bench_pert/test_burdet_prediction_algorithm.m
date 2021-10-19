36b60396065e3a27e5afd1a0c066035f4a0b84dc
Vincent Fortineau <vfortineau@free.fr>
2021-10-19 17:57:53 +0200

    Update test_burdet_prediction_algorithm.m

--- data/ball_bouncing_experiment/experimental_bench_pert/test_burdet_prediction_algorithm.m
+++ data/ball_bouncing_experiment/experimental_bench_pert/test_burdet_prediction_algorithm.m
@@ -11,7 +11,7 @@ addpath('utils');
 addpath('utils/cycles');
 addpath('Input_signal_examples/');
 addpath('trajectory_prediction_evaluation/Burdet2000');
-base_save_path = "trajectory_prediction_evaluation\Burdet2000\";
+base_save_path = "trajectory_prediction_evaluation/Burdet2000/";
 
 %%%%%%%%%%
 % MACRO
@@ -23,17 +23,44 @@ NB_TIME_DISTORTIONS = [1];%[1, 5, 9]; % should be of size NB_OF_CONFIGURATIONS
 TIME_DISTORTION_GROWTH = [0];%[0, 0.03, 0.03]; % provided as percentage
 CLEAR_TEMPORARY_DATA = 1; % Not setting this var to 1 can lead to memory crashes
 SAVE_FIGURES = 0;
-LOW_PASS_FREQ = 50; % Hz
+LOW_PASS_FREQ = 20; % Hz (50 Hz)
 
 % INPUT SIGNAL
-EXTERNAL_INPUT_SIGNAL = 0;
+EXTERNAL_INPUT_SIGNAL = 1;
+EXTERNAL_IS_FORCE = 0; % set to 0 to use position
 if EXTERNAL_INPUT_SIGNAL
     addpath('../../force_torque_sensor');
-    load("preliminary_experimental_data/data_vfo_10.mat"); 
+    %load("preliminary_experimental_data/data_vfo_10.mat"); 
+    necessary_variables = {'forces_unf','torque_unf','thetas', 'dt',...
+        'mocap_marker_robot_base','t','idx_ball_off_ramp','dist', 't_dist', ...
+        'SINGLE_MOCAP_FITTING'};
+    load("data_2020_Nov_17/data_without_impacts_2020_11_17.mat", necessary_variables{:}); 
     t_in = t;
     clear t
-    idx_start = [1.5e4];
-    idx_end = [6.625e5];   
+    %idx_start = [1.5e4];
+    %idx_end = [6.625e5];  
+    if iscell(forces_unf)
+        if SINGLE_MOCAP_FITTING % first data cell should be deleted
+            forces_unf(1) = [];
+            if exist('torque_unf') == 1 % test if it is a var in workspace
+                torque_unf(1) = [];
+            else
+                torque_unf = forces_unf;
+            end
+            thetas(1) = [];
+            mocap_marker_robot_base(1) = [];
+            idx_ball_off_ramp(1)= [];
+            t_in(1) = [];
+        else
+            % TODO
+        end
+    end
+    idx_start_arr = cell2mat(idx_ball_off_ramp);
+    idx_end_arr = cellfun(@length,forces_unf);
+    if NB_OF_GLOBAL_ITERATIONS > length(forces_unf)
+        NB_OF_GLOBAL_ITERATIONS = length(forces_unf);
+    end
+    
 else
     TIME_VARIANT_MAGN = 1;
     TIME_VARIANT_PHASE = 1;
@@ -77,9 +104,15 @@ for SIGNAL_IDX = 1:NB_OF_GLOBAL_ITERATIONS
 
 if EXTERNAL_INPUT_SIGNAL
     t = t_in{SIGNAL_IDX}';
-    [f,~,~]= forces_filtering(forces_unf{SIGNAL_IDX}', torques_unf{SIGNAL_IDX}', ...
-        thetas{SIGNAL_IDX}', t');
-    nsig = -1.*f(3,:);
+    if EXTERNAL_IS_FORCE
+        [f_int,~,~]= forces_filtering(forces_unf{SIGNAL_IDX}', torques_unf{SIGNAL_IDX}', ...
+            thetas{SIGNAL_IDX}', t');
+        nsig = -1.*f_int(3,:);
+    else % External signal is position
+        tmp = mocap_marker_robot_base{SIGNAL_IDX};
+        nsig = tmp(:,3)'; % z position
+        clear tmp
+    end
     % filtering
     fc = LOW_PASS_FREQ; % cut off frequency
     [b,a] = butter(2,fc/(1/(2*dt)),'low'); 
@@ -93,6 +126,8 @@ if EXTERNAL_INPUT_SIGNAL
     
     % to avoid the irrelevant data at both the beginning and end of the
     % signal
+    idx_start = idx_start_arr(SIGNAL_IDX);
+    idx_end = idx_end_arr(SIGNAL_IDX);
     nsig = nsig(idx_start:idx_end);
     fsig = fsig(idx_start:idx_end);
     sig = sig(idx_start:idx_end);
@@ -172,24 +207,29 @@ title("Cycles frequencies variations n" + num2str(SIGNAL_IDX))% + " _base freq "
 
 if EXTERNAL_INPUT_SIGNAL
     
-    dist_val = dist{SIGNAL_IDX};
-    dist_idx = zeros(size(t_dist{SIGNAL_IDX}));
-    del_list = [];
-    for i = 1:length(t_dist{SIGNAL_IDX})
-         tmp = find(t >= t_dist{SIGNAL_IDX}(i), 1, 'first');
-         if isempty(tmp)
-            del_list = [del_list, i];
-         else
-             dist_idx(i) = tmp;
-         end
-        if dist_val(i) == 0 % to avoid considering the end of a perturbation
-            % as no perturbation...
-            dist_val(i) = sign(dist_val(i-1));
+    if exist('dist') == 1 
+        dist_val = dist{SIGNAL_IDX};
+        dist_idx = zeros(size(t_dist{SIGNAL_IDX}));
+        del_list = [];
+        for i = 1:length(t_dist{SIGNAL_IDX})
+             tmp = find(t >= t_dist{SIGNAL_IDX}(i), 1, 'first');
+             if isempty(tmp)
+                del_list = [del_list, i];
+             else
+                 dist_idx(i) = tmp;
+             end
+            if dist_val(i) == 0 % to avoid considering the end of a perturbation
+                % as no perturbation...
+                dist_val(i) = sign(dist_val(i-1));
+            end
         end
+        % delete data that is outside the time limits
+        dist_idx(del_list) =[];
+        dist_val(del_list) =[];
+    else
+        dist_idx = [];
+        dist_val = [];
     end
-    % delete data that is outside the time limits
-    dist_idx(del_list) =[];
-    dist_val(del_list) =[];
     
     complete_cycles = splitCycles([], fsig, t, idx_pks, dist_idx, dist_val);
     j = 1;
@@ -236,20 +276,20 @@ k = 10; % mean will be computed on the 10 previous cycles
 m_nb = 15; % nb of magnitudes (should be odd to include x1 mag)
 ts_nb = 11; % nb of time shifts (should be oddto include 0 delay)
 td_nb = NB_TIME_DISTORTIONS(CONFIG_IDX); % nb of time distortions (should be odd to include x1 distortion)
-coef_std = 6; % max magnitude is determined by the std times this coeff
-max_time_delay = 40; % in terms of samples
+coef_std = 1.5; % max magnitude is determined by the std times this coeff
+max_time_delay = 60;%40; % in terms of samples
 max_time_distortion = TIME_DISTORTION_GROWTH(CONFIG_IDX); % max percentage of growth/compression
 pred_eval_idx = 200-1; % the algorithm will be evaluated on this nb of samples + 1
-offset_idx_pred = 300; % 
+offset_idx_pred = 300; %
 
-prev_cycles = ncycles(1:k);
+prev_cycles = cycles(1:k); % ncycles
 time_shift = linspace(-max_time_delay,max_time_delay,ts_nb);
 time_dist = 1+linspace(-max_time_distortion,max_time_distortion,td_nb);
 %cand = NaN(m_nb*ts_nb, length(ncycles(1).force));
 cand = NaN(m_nb*ts_nb*td_nb, min_idx); 
 
 % cycles iterations
-for i = k+1:length(ncycles)-1
+for i = k+1:length(cycles)-1 % ncycles
     
     % cycle formating, cycles length are reduced to the minimum cycle length
     cell_f = {prev_cycles.force};
@@ -298,21 +338,21 @@ for i = k+1:length(ncycles)-1
     learn_idx = (-99:0) + offset_idx_pred + rand_val; % last 100ms indexes
     for c = 1:size(cand,1)
         for idx_t = 1:offset_idx_pred + rand_val
-            d_cost(c) = nansum([(ncycles(i).force(idx_t) - cand(c,idx_t))^2, alpha*d_cost(c)]);
+            d_cost(c) = nansum([(cycles(i).force(idx_t) - cand(c,idx_t))^2, alpha*d_cost(c)]); % ncycles
         end
-        alt_cost(c) = rms(nansum([ncycles(i).force(learn_idx); -cand(c,learn_idx)],1));
+        alt_cost(c) = rms(nansum([cycles(i).force(learn_idx); -cand(c,learn_idx)],1)); % ncycles
     end
     [~, d_min_idx] = min(d_cost);
     b_cand = cand(d_min_idx,:);
     o_cand = cand(setdiff(1:end,d_min_idx),:);
     [rms_err_choice(i,2), d_min_idx] = min(alt_cost);
     alt_b_cand = cand(d_min_idx,:);
-    rms_err_choice(i,1) = rms(nansum([ncycles(i).force(learn_idx); -b_cand(learn_idx)],1));
+    rms_err_choice(i,1) = rms(nansum([cycles(i).force(learn_idx); -b_cand(learn_idx)],1)); % ncycles
     
     % prev_cycles FIFO 
     % TODO: a conditonnal evaluation of the new std should say if the new cycle
     % is added or not, according to Burdet et al. (2000)
-    prev_cycles = [prev_cycles(2:end), ncycles(i)];
+    prev_cycles = [prev_cycles(2:end), cycles(i)]; % ncycles
     pred_indexes = learn_idx(end)+(0:pred_eval_idx); % the indices for prediction
     % To compare with spline method FORTINEAU et al.(2020)
     if DO_SPLINE_INTERP && CONFIG_IDX == 1 % splines have only 1 config
@@ -333,31 +373,31 @@ for i = k+1:length(ncycles)-1
             % spline is used
             %spline_pp = interp1(ncycles(i).time(interp_indexes), ...
             %    ncycles(i).force(interp_indexes), 'cubic', 'pp');
-            spline_pp = spline(ncycles(i).time(interp_indexes), ncycles(i).force(interp_indexes));
+            spline_pp = spline(cycles(i).time(interp_indexes), cycles(i).force(interp_indexes)); % ncycles
             
             % the spleen will only be evaluated for pred_eval_idx points 
             % after the begining of the "prediction", therefore, according
             % to the landing point, indexes needs to either be deleted or
             % added using the same spline equation
-            spline_cand(i_pt, :) = ppval(spline_pp,ncycles(i).time(pred_indexes));
-            spline_cost(i_pt) = rms(nansum([ncycles(i).force(pred_indexes); -spline_cand(i_pt, :)],1));
-        end
+            spline_cand(i_pt, :) = ppval(spline_pp,cycles(i).time(pred_indexes)); % ncycles
+            spline_cost(i_pt) = rms(nansum([cycles(i).force(pred_indexes); -spline_cand(i_pt, :)],1)); % ncycles
+        end 
         [~, spline_min_idx] = min(spline_cost);
         [~, spline_max_idx] = max(spline_cost);
         w_spline_cand = spline_cand(spline_max_idx, :); % worst spline
         b_spline_cand = spline_cand(spline_min_idx, :); % best spline
         mean_spline_cand = mean(spline_cand, 1); % average of the splines
 
-        rms_err(i,3) = rms( ncycles(i).force(pred_indexes) - mean_spline_cand );
-        rms_err(i,4) = rms( ncycles(i).force(pred_indexes) - b_spline_cand );
-        rms_err(i,5) = rms( ncycles(i).force(pred_indexes) - w_spline_cand );
+        rms_err(i,3) = rms( cycles(i).force(pred_indexes) - mean_spline_cand ); % ncycles
+        rms_err(i,4) = rms( cycles(i).force(pred_indexes) - b_spline_cand ); % ncycles
+        rms_err(i,5) = rms( cycles(i).force(pred_indexes) - w_spline_cand ); % ncycles
         
-        errors(SIGNAL_IDX, CONFIG_IDX, 3).appendData(ncycles(i).force(pred_indexes) -...
-                mean_spline_cand);
-        errors(SIGNAL_IDX, CONFIG_IDX, 4).appendData(ncycles(i).force(pred_indexes) -...
-             b_spline_cand);
-        errors(SIGNAL_IDX, CONFIG_IDX, 5).appendData(ncycles(i).force(pred_indexes) -...
-             w_spline_cand);
+        errors(SIGNAL_IDX, CONFIG_IDX, 3).appendData(cycles(i).force(pred_indexes) -...
+                mean_spline_cand); % ncycles
+        errors(SIGNAL_IDX, CONFIG_IDX, 4).appendData(cycles(i).force(pred_indexes) -...
+             b_spline_cand); % ncycles
+        errors(SIGNAL_IDX, CONFIG_IDX, 5).appendData(cycles(i).force(pred_indexes) -...
+             w_spline_cand); % ncycles
         
         if DISP_SPLINE_ALL_CANDIDATES && i == 15%k+1
             figure
@@ -366,7 +406,7 @@ for i = k+1:length(ncycles)-1
                 p1(p_idx).Color(4) = 0.3;
             end
             hold on
-            p0 = plot(ncycles(i).force, 'Color', [0 0.4470 0.7410]);
+            p0 = plot(cycles(i).force, 'Color', [0 0.4470 0.7410]); % ncycles
             p2 = plot(pred_indexes, mean_spline_cand, 'k');
             p3 = plot(pred_indexes, b_spline_cand, 'Color', [0.4660, 0.6740, 0.1880]);
             p4 = plot(pred_indexes, w_spline_cand, 'Color', [0.6350, 0.0780, 0.1840]);
@@ -381,13 +421,13 @@ for i = k+1:length(ncycles)-1
     % 300ms : pred_indexes
     % validation is on the 100 samples before the prediction
     val_indexes = pred_indexes(1) + (-99:-1);
-    rms_err(i, 1) = rms( ncycles(i).force(pred_indexes) - b_cand(pred_indexes) );
-    rms_err(i, 2) = rms( ncycles(i).force(pred_indexes) - alt_b_cand(pred_indexes) );
+    rms_err(i, 1) = rms( cycles(i).force(pred_indexes) - b_cand(pred_indexes) ); % ncycles
+    rms_err(i, 2) = rms( cycles(i).force(pred_indexes) - alt_b_cand(pred_indexes) ); % ncycles
 
-    errors(SIGNAL_IDX, CONFIG_IDX, 1).appendData(ncycles(i).force([val_indexes,...
-        pred_indexes]) - b_cand([val_indexes, pred_indexes]));
-    errors(SIGNAL_IDX, CONFIG_IDX, 2).appendData(ncycles(i).force([val_indexes,...
-        pred_indexes]) - alt_b_cand([val_indexes, pred_indexes]));
+    errors(SIGNAL_IDX, CONFIG_IDX, 1).appendData(cycles(i).force([val_indexes,...
+        pred_indexes]) - b_cand([val_indexes, pred_indexes])); % ncycles
+    errors(SIGNAL_IDX, CONFIG_IDX, 2).appendData(cycles(i).force([val_indexes,...
+        pred_indexes]) - alt_b_cand([val_indexes, pred_indexes])); % ncycles
     
 %     errors(SIGNAL_IDX, CONFIG_IDX, 1) = STATISTIC_DATA(ncycles(i).force([val_indexes, pred_indexes]) -...
 %         b_cand([val_indexes, pred_indexes]), "Burdet candidate for config" + num2str(CONFIG_IDX));  
@@ -399,7 +439,7 @@ for i = k+1:length(ncycles)-1
     figure
     p1 = plot(o_cand(:,:)', 'Color', [0.3010 0.7450 0.9330]);
     hold on
-    p0 = plot(ncycles(i).force, 'Color', [0 0.4470 0.7410]);
+    p0 = plot(cycles(i).force, 'Color', [0 0.4470 0.7410]); % ncycles
     plot(b_cand, 'k')   
     p2 = plot(alt_b_cand, 'k');
     p3 = plot(1:pred_indexes(1), b_cand(1:pred_indexes(1)), 'Color', [0.4660 0.6740 0.1880]);
@@ -478,9 +518,10 @@ if CLEAR_TEMPORARY_DATA
     mean_rmse_meth_burdet std_rmse_meth_burdet mean_rmse_meth_minRmse ...
     std_rmse_meth_minRmse DISP_ALL_CANDIDATES_AND_BEST_MATCH_FOR_CYCLES ...
     errors base_save_path SAVE_FIGURES CLEAR_TEMPORARY_DATA DO_SPLINE_INTERP ...
-    DISP_SPLINE_ALL_CANDIDATES
+    DISP_SPLINE_ALL_CANDIDATES EXTERNAL_INPUT_SIGNAL RAND_GEN_LIST
 end
 end % for CONFIG_IDX = 1:NB_OF_CONFIGURATIONS
+RAND_GEN_LIST{SIGNAL_IDX} = RAND_GEN;
 % clear everything but macros and analytics
 if CLEAR_TEMPORARY_DATA
     clearvars -except NB_OF_GLOBAL_ITERATIONS NB_OF_CONFIGURATIONS NB_OF_METHODS...
@@ -488,7 +529,7 @@ if CLEAR_TEMPORARY_DATA
     CONFIG_IDX TIME_DISTORTION_GROWTH mean_rmse_meth_burdet std_rmse_meth_burdet ...
     mean_rmse_meth_minRmse std_rmse_meth_minRmse DISP_ALL_CANDIDATES_AND_BEST_MATCH_FOR_CYCLES ...
     errors base_save_path SAVE_FIGURES CLEAR_TEMPORARY_DATA DO_SPLINE_INTERP ...
-    DISP_SPLINE_ALL_CANDIDATES
+    DISP_SPLINE_ALL_CANDIDATES EXTERNAL_INPUT_SIGNAL RAND_GEN_LIST
 end
 end % for SIGNAL_IDX = 1:NB_OF_GLOBAL_ITERATIONS
 
@@ -519,3 +560,4 @@ end % for SIGNAL_IDX = 1:NB_OF_GLOBAL_ITERATIONS
 % end
 
 %save("test_prediction_trajectoire_real_force_1.mat")
+save("test_prediction_trajectoire_real_position_1.mat")
